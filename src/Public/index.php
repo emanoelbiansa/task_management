75812099d06f00ed1dc18ec0ebb1323d71d3bfe2
dzakimaulana <dzakimaulanapati@gmail.com>
2024-11-23 11:54:48 +0700

    v2.1

--- src/Public/index.php
+++ src/Public/index.php
@@ -12,7 +12,7 @@ include '../include/header.php';
 
 <main>
     <h2>Welcome to Your Task Management System</h2>
-    <p>Manage your tasks efficiently. <a href="/public/task.php">View your tasks</a></p>
+    <p>Manage your tasks efficiently. <a href="/task.php">View your tasks</a></p>
 </main>
 
 <?php
